2bd05528f170035abb86a9f5268e0d160effd22e
RajasCPatil <rajascpatil@gmail.com>
2022-10-23 06:31:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Healthy Dinner')
+streamlit.header('Beakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Rnge Egg')
