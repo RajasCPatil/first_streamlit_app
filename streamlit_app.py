a093ce3d20ccacfa4bd2e92d7dc35a730d3ac967
RajasCPatil <rajascpatil@gmail.com>
2022-10-23 06:36:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ streamlit.header('Beakfast Menu')
 streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Rnge Egg')
-streamlit.text(🥑🍞 Avacado Toast')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
